11fdcda75698a7aea46aaaf4629041c37f7e4b03
DimanaNikolova <dimanannikolova@gmail.com>
2021-12-15 23:09:53 +0200

    config

--- config/config.js
+++ config/config.js
@@ -1,13 +1,28 @@
-const env = process.env.NODE_ENV || "development";
+// const env = process.env.NODE_ENV || "development";
+
+// const config = {
+//     development: {
+//         port: process.env.PORT || 9999,
+//         dbURL: "mongodb://localhost:27017/",
+//         // dbURL: 'mongodb+srv://dimana:dimana@signupapp.lrmp7.mongodb.net/SignUpApp?retryWrites=true&w=majority',
+//         cookie: "x-auth-token"
+//     },
+//     production: {}
+// };
+
+// module.exports = config[env];
 
 const config = {
     development: {
-        port: process.env.PORT || 9999,
+        PORT: 9999,
         dbURL: "mongodb://localhost:27017/",
-        // dbURL: 'mongodb+srv://dimana:dimana@signupapp.lrmp7.mongodb.net/SignUpApp?retryWrites=true&w=majority',
         cookie: "x-auth-token"
     },
-    production: {}
+    production: {
+        PORT: process.env.PORT,
+        dbURL: "mongodb://localhost:27017/",
+        cookie: "x-auth-token"
+    },
 };
 
-module.exports = config[env];
+module.exports = config[process.env.NODE_ENV.trim()];
